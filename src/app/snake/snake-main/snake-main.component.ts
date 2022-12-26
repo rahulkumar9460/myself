b345e89ab919301451ee7cbb1e5ae4fb3e6a8459
rahulkumar9460 <66291880+rahulkumar9460@users.noreply.github.com>
2022-12-26 16:20:06 +0530

    Snake can be paused and resumed

--- src/app/snake/snake-main/snake-main.component.ts
+++ src/app/snake/snake-main/snake-main.component.ts
@@ -25,7 +25,7 @@ export class SnakeMainComponent implements OnInit {
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
-    console.log(event.key);
+    //console.log(event.key);
     if(this.keysToDirections[event.key])this.changeInputDirection(this.keysToDirections[event.key]);
   }
 
@@ -41,6 +41,7 @@ export class SnakeMainComponent implements OnInit {
   }
 
   directions: Array<string> = ['left', 'right', 'up', 'down'];
+  oppDirectionMap: any = {'left': 'right', 'right': 'left', 'up': 'down', 'down': 'up'};
   keysToDirections: any = {'a': 'left', 'd': 'right', 'w': 'up', 's': 'down', 'ArrowUp': 'up', 'ArrowDown': 'down', 'ArrowLeft': 'left', 'ArrowRight': 'right'};
   grid: any = { height: 20, width: 20 };
   gridElementSize: number = 50;
@@ -49,32 +50,62 @@ export class SnakeMainComponent implements OnInit {
 
   snake: Array<snake_body> = [];
   snakeSet: Set<string> = new Set();
-  snakeSpeed: number = 70;
+  snakeSpeed: number = 80;
+  initialLengthOfSnake: number = 10;
   inputDirection: string = 'left';
+
+  isGameOver: boolean = false;
+  isGameRunning: boolean = false;
+  interval: any = null;
 
   ngOnInit(): void {
     //console.log(window.innerHeight, window.innerWidth, this.grid.height, this.grid.width, this.gridElementSize);
-    this.fillHeightAndWidthIterators();
-    this.initSnake();
-
-    setInterval(() => {
-      //let num =  Math.floor(Math.random() * this.directions.length);
-      this.moveSnake(this.inputDirection);
-    }, this.snakeSpeed);
+    this.startGame();
   }
 
   fillHeightAndWidthIterators() {
+    //console.log("Filling iterators....");
     for (let i = 0; i < this.height_iterator.length; i++) this.height_iterator[i] = i;
     for (let i = 0; i < this.width_iterator.length; i++) this.width_iterator[i] = i;
 
   }
 
-  initSnake() {
-    let initialLengthOfSnake = 5;
-    let headY = Math.floor(this.grid.height / 2);
-    let headX = Math.floor(this.grid.width / 2 - initialLengthOfSnake / 2);
+  startGame() {
+    console.log("game starting");
+    this.isGameOver = false;
+    this.interval = null;
+    this.inputDirection = 'left';
+    this.fillHeightAndWidthIterators();
+    this.initSnake();
+    this.resumeGame();
+  }
 
-    for (let i = 0; i < initialLengthOfSnake; i++) {
+  pauseGame() {
+    this.isGameRunning = false;
+    clearInterval(this.interval);
+  }
+
+  resumeGame() {
+    //console.log("Resuming game");
+    this.isGameRunning = true;
+    this.interval = setInterval(() => {
+      //let num =  Math.floor(Math.random() * this.directions.length);
+      if(this.isGameOver || !this.isGameRunning) {
+        clearInterval(this.interval);
+      } else {
+        this.moveSnake(this.inputDirection);
+      }
+    }, this.snakeSpeed);
+  }
+
+  initSnake() {
+    //console.log("Init snake..");
+    let headY = Math.floor(this.grid.height / 2);
+    let headX = Math.floor(this.grid.width / 2 - this.initialLengthOfSnake / 2);
+
+    this.snake = new Array();
+    this.snakeSet = new Set();
+    for (let i = 0; i < this.initialLengthOfSnake; i++) {
       this.snake.push({ x: headX + i, y: headY });
       this.snakeSet.add(headX + i + '_' + headY);
     }
@@ -83,36 +114,54 @@ export class SnakeMainComponent implements OnInit {
   }
 
   isSnakeBody(x: number, y: number) {
-    //console.log(x, y);
-    return this.snakeSet.has(x + '_' + y) ? true : false;
+    if(this.snakeSet.has(x + '_' + y)) {
+      //console.log(this.snakeSet, x, y);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   changeInputDirection(dir: string) {
-    let newHead = { x: 0, y: 0 };
-    if (dir == 'left') {
-      newHead.x = this.snake[0].x - 1;
-      newHead.y = this.snake[0].y;
+    //console.log("changing input direction");
+    if(!this.isAValidInputDir(dir))return;
+    let newHead = this.calculateNewHead(dir);
+    if (this.isGameOverFun(newHead.x, newHead.y)){
+      return;
+    } else {
+      this.inputDirection = dir;
     }
-    else if (dir == 'right') {
-      newHead.x = this.snake[0].x + 1;
-      newHead.y = this.snake[0].y;
-    }
-    else if (dir == 'up') {
-      newHead.x = this.snake[0].x;
-      newHead.y = this.snake[0].y - 1;
-    }
-    else if (dir == 'down') {
-      newHead.x = this.snake[0].x;
-      newHead.y = this.snake[0].y + 1;
-    }
+  }
 
-    newHead.x = (newHead.x + this.grid.width) % this.grid.width;
-    newHead.y = (newHead.y + this.grid.height) % this.grid.height;
-    if (this.isSnakeBody(newHead.x, newHead.y)) return;
-    this.inputDirection = dir;
+  isAValidInputDir(dir: string) {
+    //console.log("Check if inputDir is valid or not... ");
+    if(this.inputDirection == dir) return false;
+    else if(this.oppDirectionMap[dir] == this.inputDirection) return false;
+    return true;
   }
 
   moveSnake(dir: string) {
+    let newHead = this.calculateNewHead(dir);
+    if(this.isGameOverFun(newHead.x, newHead.y)) {
+      return;
+    } else {
+      this.addNewHeadAndRemoveTail(newHead);
+    }
+  }
+
+  isGameOverFun(x: number, y: number) {
+    //console.log("Checking if game is over...");
+    if (this.isSnakeBody(x, y)){
+      console.log("Game over!!!!!!!!!!");
+      //console.log(this.snakeSet, x, y);
+      this.isGameOver = true;
+      this.isGameRunning = false;
+      return true;
+    }
+    return false;
+  }
+
+  calculateNewHead(dir: string) {
     let newHead = { x: 0, y: 0 };
     if (dir == 'left') {
       newHead.x = this.snake[0].x - 1;
@@ -133,28 +182,22 @@ export class SnakeMainComponent implements OnInit {
 
     newHead.x = (newHead.x + this.grid.width) % this.grid.width;
     newHead.y = (newHead.y + this.grid.height) % this.grid.height;
-    if (this.isSnakeBody(newHead.x, newHead.y)) return;
-    this.addNewHeadAndRemoveTail(newHead);
+    return newHead;
   }
 
   addNewHeadAndRemoveTail(newHead: snake_body) {
-    this.snakeSet.add(newHead.x + '_' + newHead.y);
-    this.snake.unshift(newHead);
-
     let last = this.snake[this.snake.length - 1];
     this.snakeSet.delete(last.x + '_' + last.y);
     this.snake.splice(-1);
+
+    this.snakeSet.add(newHead.x + '_' + newHead.y);
+    this.snake.unshift(newHead);
     //console.log(this.snake);
   }
 
   ifInGrid(x: number, y: number) {
     if (x < 0 || x >= this.grid.width || y < 0 || y >= this.grid.height) return false;
     return true;
-  }
-
-  onKeydown(event: any) {
-    console.log(event.key);
-    window.alert(event.key);
   }
 
 }
